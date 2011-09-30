2f2e17f3b322dc427e13cb0d0bf0dbe73d615fd9
Carlos Oliveros <carlos.oliveros@koombea.com>
2011-09-30 16:50:51 -0500

    Test, without deleted files

--- db/migrate/20110927033619_add_username_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :userrol, :string
-  end
-
-  def self.down
-    remove_column :users, :userrol
-  end
-end
--- db/migrate/20110929215133_remove_type_to_person.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTypeToPerson < ActiveRecord::Migration
-  def self.up
-    remove_column :people, :type
-  end
-
-  def self.down
-    add_column :people, :type, :string
-  end
-end
